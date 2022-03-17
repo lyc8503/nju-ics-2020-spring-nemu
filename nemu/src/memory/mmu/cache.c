51294d90c581dfceeddfc4ef4fadfc02e9dea10b
tracer-pa-public <tracer@njuics.org>
2022-03-17 15:04:16 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 15:04:16 up 2 days, 16:52,  2 users,  load average: 0.00, 0.01, 0.00 > unitime > 80335016dc17c684520b52c53a0ae825b115f98

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -1,27 +1,88 @@
 #include "memory/mmu/cache.h"
 
-#define CACHE_BLOCK_SIZE 64
-#define CACHE_BLOCK_NUM 1024
 
-uint8_t l1cache[CACHE_BLOCK_NUM][CACHE_BLOCK_SIZE];
-uint8_t valid_bits[CACHE_BLOCK_NUM];
+#define LINE_SIZE 8
+#define BLOCK_SIZE 8
+#define CACHE_SIZE 1024
+
+typedef struct CACHE_LINE {
+    union DATA {
+        uint8_t data[LINE_SIZE];
+        uint32_t data32[LINE_SIZE / 4];
+    } d;
+    uint32_t addr;
+} CACHE_LINE;
+
+typedef struct CACHE_BLOCK {
+    CACHE_LINE lines[BLOCK_SIZE];
+} CACHE_BLOCK;
+
+typedef struct L1CACHE {
+    CACHE_BLOCK blocks[CACHE_SIZE];
+} L1CACHE;
+
+L1CACHE cache;
 
 // init the cache
 void init_cache()
 {
-    memset(valid_bits, 0, CACHE_BLOCK_NUM / 8);
+    for (int i = 0; i < CACHE_SIZE; i++) {
+        for (int j = 0; j < BLOCK_SIZE; j++) {
+            cache.blocks[i].lines[j].addr = 0xffffffff;
+        }
+    }
 }
 
 // write data to cache
 void cache_write(paddr_t paddr, size_t len, uint32_t data)
 {
+
+
 	// implement me in PA 3-1
 }
 
 // read data from cache
 uint32_t cache_read(paddr_t paddr, size_t len)
 {
-	// implement me in PA 3-1
-	return 0;
+    uint32_t t = paddr / (MEM_SIZE_B / CACHE_SIZE);
+    CACHE_BLOCK b = cache.blocks[t];
+
+    uint32_t hit_flag = 0;
+    uint32_t data = 0;
+
+    for (int i = 0; i < BLOCK_SIZE; i++) {
+        if (paddr >= b.lines[i].addr && paddr + len < b.lines[i].addr + LINE_SIZE) {
+            hit_flag = 1;
+            for (int j = 0; j < len; j++) {
+                data |= (b.lines[i].d.data[paddr - b.lines[i].addr] << (j - 1) * 8);
+            }
+            break;
+        }
+    }
+
+    // cache miss
+//    if (!hit_flag) {
+//        // hw read
+//        uint32_t data = hw_mem_read(paddr, 4);
+//        uint32_t data2 = hw_mem_read(paddr + 1, 4);
+//
+//        // cache
+//        for (int i = 0; i < BLOCK_SIZE; i++) {
+//            if (b.lines[i].addr == 0xffffffff) {
+//                break;
+//            }
+//        }
+//
+//        if (i < BLOCK_SIZE) {
+//            // not full
+//            b.lines[i].addr == paddr;
+//            b.lines[i].DATA32[0] == ;
+//        } else {
+//            // full
+//
+//        }
+//    }
+
+	return data;
 }
 
