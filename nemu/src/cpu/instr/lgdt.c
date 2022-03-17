d59ea99688c7a0fe0fb568ff4452cae2fcdeaa02
tracer-pa-public <tracer@njuics.org>
2022-03-17 23:43:25 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 23:43:25 up 3 days,  1:31,  2 users,  load average: 0.00, 0.02, 0.00 > unitime > bcda8ebbd908e7f279f3158df67fbed1c854ec08

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -14,20 +14,20 @@ make_instr_func(lgdt_instr) {
 //
 //    operand_read(&m);
 
-//    cpu.gdtr.limit = paddr_read(cpu.eip + 1, 2);
-//    cpu.gdtr.base = paddr_read(cpu.eip + 3, 4);
-    OPERAND m;
-    m.data_size = 16;
-    int len = 1;
-    len += modrm_rm(eip + 1, &m);
-    operand_read(&m);
-    cpu.gdtr.limit = m.val;
-
-    m.data_size = 32;
-    m.addr += 2;
-    operand_read(&m);
-
-    cpu.gdtr.base = m.val;
+    cpu.gdtr.limit = paddr_read(cpu.eip + 1, 2);
+    cpu.gdtr.base = paddr_read(cpu.eip + 3, 4);
+//    OPERAND m;
+//    m.data_size = 16;
+//    int len = 1;
+//    len += modrm_rm(eip + 1, &m);
+//    operand_read(&m);
+//    cpu.gdtr.limit = m.val;
+//
+//    m.data_size = 32;
+//    m.addr += 2;
+//    operand_read(&m);
+//
+//    cpu.gdtr.base = m.val;
 
     trace_instr("lgdt base %x limit %x", m.val, cpu.gdtr.limit);
 
