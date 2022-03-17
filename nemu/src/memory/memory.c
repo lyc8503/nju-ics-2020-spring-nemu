5636c858b8d5d90b27244cfd91106b5400837287
tracer-pa-public <tracer@njuics.org>
2022-03-17 14:13:40 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 14:13:40 up 2 days, 16:01,  2 users,  load average: 0.00, 0.01, 0.00 > unitime > e9ce5506e2373e492a51498bfcb8a13c34ec6692

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -19,16 +19,22 @@ void hw_mem_write(paddr_t paddr, size_t len, uint32_t data)
 	memcpy(hw_mem + paddr, &data, len);
 }
 
-uint32_t paddr_read(paddr_t paddr, size_t len)
-{
-	uint32_t ret = 0;
-	ret = hw_mem_read(paddr, len);
-	return ret;
+uint32_t paddr_read(paddr_t paddr, size_t len) {
+    uint32_t ret = 0;
+#ifdef CACHE_ENABLED
+    ret = cache_read(paddr, len, &L1_dcache);
+#else
+    ret = hw_mem_read(paddr, len);
+#endif
+    return ret;
 }
 
-void paddr_write(paddr_t paddr, size_t len, uint32_t data)
-{
-	hw_mem_write(paddr, len, data);
+void paddr_write(paddr_t paddr, size_t len, uint32_t data) {
+#ifdef CACHE_ENABLED
+    cache_write(paddr, len, data, &L1_dcache);
+#else
+    hw_mem_write(paddr, len, data);
+#endif
 }
 
 uint32_t laddr_read(laddr_t laddr, size_t len)
