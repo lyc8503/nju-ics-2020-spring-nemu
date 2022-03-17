ac0b46453c29b0457913fd956547322212a2ee32
tracer-pa-public <tracer@njuics.org>
2022-03-17 16:00:47 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 16:00:47 up 2 days, 17:48,  2 users,  load average: 0.28, 0.09, 0.02 > unitime > d3530f21a718053985384caf5c74c1fe14e1492

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -30,6 +30,14 @@ typedef struct L1CACHE {
 
 L1CACHE cache;
 
+
+uint32_t hw_mem_read(paddr_t paddr, size_t len)
+{
+    uint32_t ret = 0;
+    memcpy(&ret, hw_mem + paddr, len);
+    return ret;
+}
+
 // init the cache
 void init_cache() {
     for (int i = 0; i < CACHE_SIZE; i++) {
@@ -101,7 +109,7 @@ uint32_t cache_read(paddr_t paddr, size_t len) {
         return cache_read(paddr, len);
     }
 
-    trace_cache("read got %x", ret);
+    trace_cache("read got %x expect %x", ret, hw_mem_read(paddr, len));
 
     return ret;
 }
