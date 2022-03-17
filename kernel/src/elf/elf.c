a26d06b782c0798bacbf92591601f5fcae067f95
tracer-pa-public <tracer@njuics.org>
2022-03-17 13:17:18 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 13:17:18 up 2 days, 15:05,  2 users,  load average: 0.00, 0.00, 0.00 > unitime > 6309f94b222ecb2cfe40343ef82b7352dc084e88

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -37,7 +37,7 @@ uint32_t loader()
 		if (ph->p_type == PT_LOAD)
 		{
 		    memcpy((void*) ph->p_vaddr, (void*) ph->p_offset, ph->p_filesz);
-		    memset((void*) (ph->p_vaddr + ph->p_filesz), 0,  p_memsz - p_filesz);
+		    memset((void*) (ph->p_vaddr + ph->p_filesz), 0,  ph->p_memsz - ph->p_filesz);
 /* TODO: copy the segment from the ELF file to its proper memory area */
 
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
