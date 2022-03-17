5861227c88953be85c5e5af7320fa76d0d846a04
tracer-pa-public <tracer@njuics.org>
2022-03-17 22:41:14 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 22:41:14 up 3 days, 29 min,  2 users,  load average: 0.01, 0.03, 0.00 > unitime > a53d6dc02f7ce85a7024048b12d8add45f77748c

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -24,7 +24,7 @@ void load_sreg(uint8_t sreg)
 	seg.val[0] = paddr_read(cpu.gdtr.base + reg->index * 8, 4);
 	seg.val[1] = paddr_read(cpu.gdtr.base + reg->index * 8 + 4, 4);
 
-	printf("load sreg %x", seg.base_15_0);
+	printf("load sreg %d\n", seg.base_15_0);
 
 	reg->base = seg.base_15_0 | (seg.base_23_16 << 16) | (seg.base_31_24) << 24;
 	reg->limit = seg.limit_15_0 | (seg.limit_19_16) << 16;
