a2f597c221c4af44f4c9ee2ca26f019b3545e4f0
tracer-pa-public <tracer@njuics.org>
2022-03-17 20:40:33 +0800

    > clean > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 20:40:33 up 2 days, 22:28,  2 users,  load average: 0.00, 0.00, 0.00 > unitime > 623ce99f62b520bf2fb399037f8d35c543962015

--- include/config.h
+++ include/config.h
@@ -5,7 +5,7 @@
 
 // PA 3
 #define CACHE_ENABLED
-//#define IA32_SEG             // protect mode enable
+#define IA32_SEG             // protect mode enable
 //#define IA32_PAGE            // virtual memory management is now complete
 //#define TLB_ENABLED
 
