81e109db512eecc6e630988569569dcedee45f3a
tracer-pa-public <tracer@njuics.org>
2022-03-17 23:42:51 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 23:42:51 up 3 days,  1:30,  2 users,  load average: 0.01, 0.03, 0.00 > unitime > 9fe5f872e15495b2b7ec679a96cf637321a30c8

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -17,18 +17,14 @@ make_instr_func(lgdt_instr) {
 //    cpu.gdtr.limit = paddr_read(cpu.eip + 1, 2);
 //    cpu.gdtr.base = paddr_read(cpu.eip + 3, 4);
     OPERAND m;
-
     m.data_size = 16;
     int len = 1;
     len += modrm_rm(eip + 1, &m);
-
     operand_read(&m);
-
     cpu.gdtr.limit = m.val;
 
     m.data_size = 32;
     m.addr += 2;
-
     operand_read(&m);
 
     cpu.gdtr.base = m.val;
