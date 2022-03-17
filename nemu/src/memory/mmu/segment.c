b10bd6c8093e2551a3dd063edac6055e97f7b5ac
tracer-pa-public <tracer@njuics.org>
2022-03-17 22:34:35 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 22:34:35 up 3 days, 22 min,  2 users,  load average: 0.00, 0.00, 0.00 > unitime > 79c93732410bd66c284c5ba654df92c333935f55

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -17,6 +17,9 @@ void load_sreg(uint8_t sreg)
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
 	SegDesc seg;
+
+	printf("load sreg index %d", cpu.segReg[sreg].index);
+
 	seg.val[0] = paddr_read(cpu.gdtr.base + cpu.segReg[sreg].index * 8, 4);
 	seg.val[1] = paddr_read(cpu.gdtr.base + cpu.segReg[sreg].index * 8 + 4, 4);
 
