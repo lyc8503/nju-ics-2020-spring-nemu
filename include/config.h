5b12b5e34cdaf4811b26de0709792260c3657841
tracer-pa-public <tracer@njuics.org>
2022-03-17 14:14:27 +0800

    > clean > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 14:14:27 up 2 days, 16:02,  2 users,  load average: 0.00, 0.01, 0.00 > unitime > ffb13c1c6e7a05bba92283034cf508b9a03bb33b

--- include/config.h
+++ include/config.h
@@ -4,7 +4,7 @@
 // #define NEMU_REF_INSTR
 
 // PA 3
- #define CACHE_ENABLED
+#define CACHE_ENABLED
 //#define IA32_SEG             // protect mode enable
 //#define IA32_PAGE            // virtual memory management is now complete
 //#define TLB_ENABLED
