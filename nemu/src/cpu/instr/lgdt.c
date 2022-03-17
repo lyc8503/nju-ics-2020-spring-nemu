223198c9e8e116446728d950e1d5f5e08dffd770
tracer-pa-public <tracer@njuics.org>
2022-03-17 23:32:05 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 23:32:05 up 3 days,  1:20,  2 users,  load average: 0.01, 0.03, 0.00 > unitime > ff17d72442f0c4fe6c5659063a7744057b4d8dcf

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -8,11 +8,17 @@ Put the implementations of `lgdt' instructions here.
 
 make_instr_func(lgdt_instr) {
 
-    cpu.gdtr.limit = paddr_read(cpu.eip + 1, 2);
-    cpu.gdtr.base = paddr_read(cpu.eip + 3, 4);
+    OPERAND m;
+    m.data_size = data_size;
+    int len = modrm_rm(&m);
 
-    trace_instr("limit %x base %x", cpu.gdtr.limit, cpu.gdtr.base);
+    operand_read(&m);
 
-    return 6;
+//    cpu.gdtr.limit = paddr_read(cpu.eip + 1, 2);
+//    cpu.gdtr.base = paddr_read(cpu.eip + 3, 4);
+
+    trace_instr("lgdt %x", m.val);
+
+    return 1 + len;
 }
 
