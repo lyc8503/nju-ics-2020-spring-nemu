43a97396ead82483fd1b5b1b2002938de2c588dc
tracer-pa-public <tracer@njuics.org>
2022-03-17 22:53:00 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 22:53:00 up 3 days, 41 min,  2 users,  load average: 0.02, 0.03, 0.00 > unitime > 402f30b25f8c5f5833256713a342aae5159a5cc4

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -10,8 +10,6 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 	return cpu.segReg[sreg].base + offset;
 }
 
-extern uint8_t hw_mem[];
-
 // load the invisible part of a segment register
 void load_sreg(uint8_t sreg)
 {
@@ -19,20 +17,20 @@ void load_sreg(uint8_t sreg)
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
 	SegReg *reg = &cpu.segReg[sreg];
-//	SegDesc seg;
+	SegDesc seg;
 
-//	seg.val[0] = paddr_read(cpu.gdtr.base + reg->index * 8, 4);
-//	seg.val[1] = paddr_read(cpu.gdtr.base + reg->index * 8 + 4, 4);
+	seg.val[0] = paddr_read(cpu.gdtr.base + reg->index * 8, 4);
+	seg.val[1] = paddr_read(cpu.gdtr.base + reg->index * 8 + 4, 4);
 
     SegDesc *seg = (SegDesc*) ((uint32_t) hw_mem + cpu.gdtr.base + reg->index * 8);
 
-	reg->base = seg->base_15_0 | (seg->base_23_16 << 16) | (seg->base_31_24) << 24;
-	reg->limit = seg->limit_15_0 | (seg->limit_19_16) << 16;
-	reg->type = seg->type;
-	reg->privilege_level = seg->privilege_level;
-	reg->soft_use = seg->soft_use;
+	reg->base = seg.base_15_0 | (seg.base_23_16 << 16) | (seg.base_31_24) << 24;
+	reg->limit = seg.limit_15_0 | (seg.limit_19_16) << 16;
+	reg->type = seg.type;
+	reg->privilege_level = seg.privilege_level;
+	reg->soft_use = seg.soft_use;
 
-	printf("load base sreg limit %d granu %d %d\n", reg->base, reg->limit, seg->granularity);
+	printf("load sreg base %d limit %d granu %d \n", reg->base, reg->limit, seg.granularity);
 
-	assert(reg->base == 0 && reg->limit == 1 && seg->granularity == 1);
+	assert(reg->base == 0 && reg->limit == 1 && seg.granularity == 1);
 }
