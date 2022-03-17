6685cedec89404cab9097473259a4c1f8ea949ee
tracer-pa-public <tracer@njuics.org>
2022-03-17 23:40:13 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 23:40:13 up 3 days,  1:28,  2 users,  load average: 0.13, 0.05, 0.02 > unitime > 1cac68b3e9167063509b55ab294c78a3338054

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -8,16 +8,32 @@ Put the implementations of `lgdt' instructions here.
 
 make_instr_func(lgdt_instr) {
 
-    OPERAND m;
-    m.data_size = data_size;
-    int len = modrm_rm(cpu.eip + 1, &m);
-
-    operand_read(&m);
+//    OPERAND m;
+//    m.data_size = data_size;
+//    int len = modrm_rm(cpu.eip + 1, &m);
+//
+//    operand_read(&m);
 
 //    cpu.gdtr.limit = paddr_read(cpu.eip + 1, 2);
 //    cpu.gdtr.base = paddr_read(cpu.eip + 3, 4);
+    OPERAND m;
 
-    trace_instr("lgdt %x", m.val);
+    m.data_size = 16;
+    int len = 1;
+    len += modrm_rm(eip + 1, &m);
+
+    operand_read(&m);
+
+    cpu.gdtr.limit = m.val;
+
+    m.data_size = 32;
+    m.addr += 2;
+
+    operand_read(&m);
+
+    cpu.gdtr.base = m.val;
+
+    trace_instr("lgdt base %x", m.val);
 
     return 1 + len;
 }
