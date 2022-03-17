d6b0606560457c31fe2d95e0d666ed7982ddcb01
tracer-pa-public <tracer@njuics.org>
2022-03-17 20:54:49 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 20:54:49 up 2 days, 22:42,  2 users,  load average: 0.03, 0.04, 0.01 > unitime > bd10030f7c7b6f90e8ed5d8c7209522d88d4310f

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -60,38 +60,38 @@ typedef struct
 //	    uint32_t base :32;
 //	} GDTR;
 
-	typedef union {
-	    struct {
-	        uint32_t pe :1;
-	        uint32_t mp :1;
-	        uint32_t em :1;
-	        uint32_t ts :1;
-	        uint32_t et :1;
-	        uint32_t reserve :26;
-	        uint32_t pg :1;
-	    };
-	    uint32_t val;
-	}CR0;
-
-	typedef struct {
-	    // the 16-bit visible part, i.e., the selector
-	    union {
-	        uint16_t val;
-	        struct {
-	            uint32_t rpl :2;
-	            uint32_t ti :1;
-	            uint32_t index :13;
-	        };
-	    };
-	    // the invisible part, i.e., cache part
-	    struct {
-	        uint32_t base;
-	        uint32_t limit;
-	        uint32_t type :5;
-	        uint32_t privilege_level :2;
-	        uint32_t soft_use :1;
-	    };
-	}SegReg;
+//	typedef union {
+//	    struct {
+//	        uint32_t pe :1;
+//	        uint32_t mp :1;
+//	        uint32_t em :1;
+//	        uint32_t ts :1;
+//	        uint32_t et :1;
+//	        uint32_t reserve :26;
+//	        uint32_t pg :1;
+//	    };
+//	    uint32_t val;
+//	}CR0;
+//
+//	typedef struct {
+//	    // the 16-bit visible part, i.e., the selector
+//	    union {
+//	        uint16_t val;
+//	        struct {
+//	            uint32_t rpl :2;
+//	            uint32_t ti :1;
+//	            uint32_t index :13;
+//	        };
+//	    };
+//	    // the invisible part, i.e., cache part
+//	    struct {
+//	        uint32_t base;
+//	        uint32_t limit;
+//	        uint32_t type :5;
+//	        uint32_t privilege_level :2;
+//	        uint32_t soft_use :1;
+//	    };
+//	}SegReg;
 
 	GDTR gdtr; // GDTR, todo: define type GDTR
 	// segment registers, todo: define type SegReg
