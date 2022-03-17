c8e52bad171226953bc0d9404653d037ed4ded34
tracer-pa-public <tracer@njuics.org>
2022-03-17 15:28:08 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 15:28:08 up 2 days, 17:16,  2 users,  load average: 0.00, 0.01, 0.00 > unitime > a5d226a96cb7987c199830f3a82ede4e6b7e0202

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -103,7 +103,7 @@ uint32_t cache_read(paddr_t paddr, size_t len) {
 
         if (i < BLOCK_SIZE) {
             // not full
-            trace_cache("not full", ret);
+            trace_cache("not full");
 
             memcpy(b.lines[i].data, hw_mem + paddr, LINE_SIZE);
             b.lines[i].addr = paddr;
