9fb080034f8728ca6d9d863cbb2b43e0a10b8430
tracer-pa-public <tracer@njuics.org>
2022-03-17 13:11:49 +0800

    > clean > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 13:11:49 up 2 days, 14:59,  2 users,  load average: 0.00, 0.02, 0.00 > unitime > 6a51a6b6d7d7c8e0a4ce3406aa4f5a2ecfa0e43f

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -37,9 +37,13 @@ uint32_t loader()
 		if (ph->p_type == PT_LOAD)
 		{
 
-			// remove this panic!!!
-			panic("Please implement the loader");
-
+		    for (uint32_t i = 0; i < ph->p_memsz; i++) {
+		        if (i < ph->filesz) {
+		            paddr_write(p_vaddr + i, 1, paddr_read(ph->p_offset + i, 1));
+		        } else {
+		            paddr_write(p_vaddr + i, 1, 0);
+		        }
+		    }
 /* TODO: copy the segment from the ELF file to its proper memory area */
 
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
