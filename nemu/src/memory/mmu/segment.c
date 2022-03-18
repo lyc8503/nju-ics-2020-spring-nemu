5eb827a7bf252c8ac4a49c7fee725297fdebc972
tracer-pa-public <tracer@njuics.org>
2022-03-18 09:53:27 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 09:53:27 up 3 days, 11:41,  2 users,  load average: 0.13, 0.06, 0.01 > unitime > 8556ea75853eb5e4aec1baddf5dfe3cae151d86f

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -22,7 +22,7 @@ void load_sreg(uint8_t sreg)
 	seg.val[0] = paddr_read(cpu.gdtr.base + reg->index * 8, 4);
 	seg.val[1] = paddr_read(cpu.gdtr.base + reg->index * 8 + 4, 4);
 
-	printf("load sreg data %08x %08x\n", seg.val[0], seg.val[1]);
+//	printf("load sreg data %08x %08x\n", seg.val[0], seg.val[1]);
 
 	reg->base = seg.base_15_0 | (seg.base_23_16 << 16) | (seg.base_31_24) << 24;
 	reg->limit = seg.limit_15_0 | (seg.limit_19_16) << 16;
@@ -30,7 +30,7 @@ void load_sreg(uint8_t sreg)
 	reg->privilege_level = seg.privilege_level;
 	reg->soft_use = seg.soft_use;
 
-	printf("load sreg base %d limit %d granu %d \n", reg->base, reg->limit, seg.granularity);
+//	printf("load sreg base %d limit %d granu %d \n", reg->base, reg->limit, seg.granularity);
 
 	assert(reg->base == 0 && reg->limit == 0xfffff && seg.granularity == 1);
 }
