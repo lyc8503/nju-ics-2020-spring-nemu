a92be8004e0306964e96af3bbac7d09985881346
tracer-pa-public <tracer@njuics.org>
2022-03-18 17:45:10 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 17:45:10 up 3 days, 19:33,  2 users,  load average: 0.07, 0.06, 0.02 > unitime > 6062b5c4e75839162cccf205684a850fc3347ac4

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -3,14 +3,15 @@
 #include "memory.h"
 #include <string.h>
 
-PDE kpdir[NR_PDE] = {0}, align_to_page;				// kernel page directory
-PTE kptable[PHY_MEM / PAGE_SIZE] align_to_page; // kernel page tables
 
 PDE *get_kpdir() { return kpdir; }
 
 /* set up page tables for kernel */
 void init_page(void)
 {
+    PDE kpdir[NR_PDE] align_to_page;				// kernel page directory
+    PTE kptable[PHY_MEM / PAGE_SIZE] align_to_page; // kernel page tables
+
 	CR0 cr0;
 	CR3 cr3;
 	PDE *pdir = (PDE *)va_to_pa(kpdir);
