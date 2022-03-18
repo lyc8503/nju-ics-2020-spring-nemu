96c33964a7529b35cacf2a66cf1d158451a0849f
tracer-pa-public <tracer@njuics.org>
2022-03-18 16:59:32 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user lyc > uname Linux debian-ics 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux > uptime 16:59:32 up 3 days, 18:47,  2 users,  load average: 0.07, 0.07, 0.02 > unitime > 3f470a96c973ae49410de24c514e30e2f0731ea5

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -6,12 +6,13 @@ paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
     PDE pde;
+    printf("page translate laddr %x\n", laddr);
     pde.val = paddr_read(cpu.cr3.val + (laddr & 0xffe00000 >> 22) * 4, 4);
-    printf("page translate pde %x", pde.val);
+    printf("page translate pde %x\n", pde.val);
     assert(pde.present);
     PTE pte;
     pte.val = paddr_read(pde.page_frame * 4096 + (laddr & 0x1ff800 >> 11) * 4, 4);
-    printf("page translate pte %x", pte.val);
+    printf("page translate pte %x\n", pte.val);
     assert(pte.present);
     uint32_t paddr = pte.page_frame * 4096 + (laddr & 0x7ff);
     return paddr;
